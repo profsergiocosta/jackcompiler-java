7a797776e8107c6393793131f70a8c0062e2d7c4
sergio costa <sergio.costa@ufma.br>
2022-05-23 08:33:01 -0300

    refatorando

--- src/main/java/br/ufma/ecp/token/IntegerToken.java
+++ src/main/java/br/ufma/ecp/token/IntegerToken.java
@@ -5,7 +5,7 @@ public class IntegerToken extends Token {
     String lexeme;
 
     public IntegerToken(String lexeme, int line) {
-        super(TokenType.NUMBER, line);
+        super(TokenType.INTEGER, line);
         this.lexeme = lexeme;
     }
 
