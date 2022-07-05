c5816d134687e32027f21cc4c83da837d0d675c9
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2022-07-05 08:29:11 -0300

    Update App.java

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,27 +1,38 @@
 package br.ufma.ecp;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
-import br.ufma.ecp.token.Token;
-import br.ufma.ecp.token.TokenType;
-
-
-class Pessoa {
-    String name;
-    public Pessoa (String name) {
-        this.name = name;
-    }
-}
 
 public class App 
 {
 
 
+    public static void saveToFile(String fileName, String output) {
+  
+       
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(fileName);
+            byte[] strToBytes = output.getBytes();
+            outputStream.write(strToBytes);
+    
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-    private static String fromFile() {
-        File file = new File("Main.jack");
+
+    private static String fromFile(File file) {        
 
         byte[] bytes;
         try {
@@ -34,26 +45,59 @@ public class App
         return "";
     } 
 
-    public static void main( String[] args )
-    {
 
-        String s1 = "Ola Mundo";
-        String s2 = new String ("Ola Mundo");
-
-        if (s1.equals(s2)) {
-            System.out.println("ok");
-        } else {
-            System.out.println("deu ruim");
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide a single file path argument.");
+            System.exit(1);
         }
 
-        Pessoa p1 = new Pessoa ("joão");
-        Pessoa p2 = new Pessoa ("joão");
+        File file = new File(args[0]);
 
-        if (p1 == p2) {
-            System.out.println("ok");
-        } else {
-            System.out.println("p1 e p2 sao diferentes");
+        if (!file.exists()) {
+            System.err.println("The file doesn't exist.");
+            System.exit(1);
         }
 
+        // we need to compile every file in the directory
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                if (f.isFile() && f.getName().endsWith(".jack")) {
+
+                    var inputFileName = f.getAbsolutePath();
+                    var pos = inputFileName.indexOf('.');
+                    var outputFileName = inputFileName.substring(0, pos) + ".vm";
+                    
+                    System.out.println("compiling " +  inputFileName);
+                    var input = fromFile(f);
+                    var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+                    parser.parse();
+                    var result = parser.VMOutput();
+                    saveToFile(outputFileName, result);
+                }
+
+            }
+        // we only compile the single file
+        } else if (file.isFile()) {
+            if (!file.getName().endsWith(".jack"))  {
+                System.err.println("Please provide a file name ending with .jack");
+                System.exit(1);
+            } else {
+                var inputFileName = file.getAbsolutePath();
+                var pos = inputFileName.indexOf('.');
+                var outputFileName = inputFileName.substring(0, pos) + ".vm";
+                
+                System.out.println("compiling " +  inputFileName);
+                var input = fromFile(file);
+                var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+                parser.parse();
+                var result = parser.VMOutput();
+                saveToFile(outputFileName, result);
+                
+            }
+        }
     }
+
+
+
 }
